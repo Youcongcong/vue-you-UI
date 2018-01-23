6d766914f8431d8babcbb7f1093b02ba4e1b973a
游聪聪 <youcongcong@ascs.tech>
2018-01-23 18:47:21 +0800

    feat:整体组件文件与事例结构搭好

--- examples/App.vue
+++ examples/App.vue
@@ -1,23 +1,39 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+    @import "../packages/style/common/normalize.less";
 </style>
+<style scoped>
+nav { margin-bottom: 40px; }
+ul { display: flex; flex-wrap: wrap; }
+li { display: inline-block; }
+li + li { border-left: solid 1px #bbb; padding-left: 10px; margin-left: 10px; }
+.container{ padding: 10px 40px 0; }
+.v-link-active { color: #bbb; }
+</style>
+<template>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><router-link to="/button">Button</router-link></li>
+            </ul>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+
+            }
+        },
+        mounted: function() {
+
+        },
+        beforeDestroy: function() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
--- examples/router/index.js
+++ examples/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+import button from '../components/button.vue';
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/button',
+            component: button,
+        }
+    ]
 })
