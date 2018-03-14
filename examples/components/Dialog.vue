539321fe65d3e395e4a469b30eadf1a717e44dd0
youcongcong <834153504@qq.com>
2018-03-14 11:00:45 +0800

    👍Perfect dialog component

--- examples/components/Dialog.vue
+++ examples/components/Dialog.vue
@@ -2,9 +2,12 @@
     <div>
         <div class="header">
             <div class="left" @click="goback"><</div>
-        <div class="title">button</div>
+        <div class="title">Dialog</div>
         </div>
-        <Button @click="alert">Default</Button>
+        <Button @click="alert">confim</Button>
+        <Button @click="alert1">无标题无取消</Button>
+        <Button @click="alert2">无取消</Button>
+        
        
     </div>
 </template>
@@ -16,9 +19,37 @@
             },
             alert(){
                 this.$dialog.Dialog.confirm({
-                    title: '1',
-                    message:'2'
+                    title:'提示' ,
+                    message:'成功',
+                    confirmButtonText:'OK'
+                }).then(() => {
+                    // on confirm
+                    console.log('ok')
+                }).catch(() => {
+                    // on cancel
+                    console.log('cancel')
                 });
+            },
+            alert1(){
+                this.$dialog.Dialog.alert({
+                    message:'2',
+                    confirmButtonText:'OK'
+                }).then(() => {
+                    // on confirm
+                    console.log('ok')
+                })
+                
+            },
+            alert2(){
+                this.$dialog.Dialog.alert({
+                    title:'提示' ,
+                    message:'2',
+                    confirmButtonText:'OK'
+                }).then(() => {
+                    // on confirm
+                    console.log('ok')
+                })
+                
             }
         }
     }
--- packages/components/dialog/dialog.vue
+++ packages/components/dialog/dialog.vue
@@ -1,140 +1,74 @@
 <template>
-  <transition name="van-dialog-bounce">
-    <div class="van-dialog" v-show="value">
-      <div class="van-dialog__header" v-if="title" v-text="title" />
-      <div class="van-dialog__content van-hairline">
-        <slot>
-          <div class="van-dialog__message" v-if="message" :class="{ 'van-dialog__message--withtitle': title }" v-html="message" />
-        </slot>
+  <div>
+    <transition name="yui-dialog-bounce">
+      <div class="yui-dialog" v-show="value">
+        <div class="yui-dialog__header" v-if="title" v-text="title" />
+        <div class="yui-dialog__content yui-hairline">
+          <slot>
+            <div class="yui-dialog__message" v-if="message" :class="{ 'yui-dialog__message--withtitle': title }" v-html="message" />
+          </slot>
+        </div>
+        <div class="yui-dialog__footer" :class="{ 'is-twobtn': showCancelButton && showConfirmButton }">
+          <Button size="large" class="yui-dialog__cancel" v-show="showCancelButton" @click="handleAction('cancel')">
+            {{ cancelButtonText}}
+          </Button>
+          <Button size="large" class="yui-dialog__confirm" :class="{ 'yui-hairline--left': showCancelButton && showConfirmButton }" v-show="showConfirmButton" @click="handleAction('confirm')">
+            {{ confirmButtonText }}
+          </Button>
+        </div>
       </div>
-      <div class="van-dialog__footer" :class="{ 'is-twobtn': showCancelButton && showConfirmButton }">
-        <Button
-          size="large"
-          class="van-dialog__cancel"
-          v-show="showCancelButton"
-          @click="handleAction('cancel')"
-        >
-          {{ cancelButtonText}}
-        </Button>
-        <Button
-          size="large"
-          class="van-dialog__confirm"
-          :class="{ 'van-hairline--left': showCancelButton && showConfirmButton }"
-          v-show="showConfirmButton"
-          @click="handleAction('confirm')"
-        >
-          {{ confirmButtonText }}
-        </Button>
-      </div>
-    </div>
-  </transition>
+    </transition>
+    <transition name="yui-fade">
+      <div class="yui-modal" v-show="value"></div>
+    </transition>
+  </div>
 </template>
 
-<script>
-
-export default {
-  name: 'Dialog',
-  props: {
-    title: String,
-    message: String,
-    callback: Function,
-    confirmButtonText: String,
-    cancelButtonText: String,
-    showCancelButton: Boolean,
-    showConfirmButton: {
-      type: Boolean,
-      default: true
-    },
-    overlay: {
-      type: Boolean,
-      default: true
-    },
-    closeOnClickOverlay: {
-      type: Boolean,
-      default: false
-    },
-    lockOnScroll: {
-      type: Boolean,
-      default: true
-    }
-  },
-
-  methods: {
-    handleAction(action) {
-      this.$emit('input', false);
-      this.$emit(action);
-      this.callback && this.callback(action);
-    }
-  }
-};
-</script>
 <style lang="less">
+  @import './dialog.less';
+</style>
 
-.van-dialog {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  width: 85%;
-  font-size: 16px;
-  overflow: hidden;
-  transition: .2s;
-  border-radius: 4px;
-  background-color: #fff;
-  transform: translate3d(-50%, -50%, 0);
+<script>
+  export default {
+    name: 'Dialog',
+    props: {
+      value: false,
+      title: String,
+      message: String,
+      callback: Function,
+      confirmButtonText: {
+        type: String,
+        default: '确定'
+      },
+      cancelButtonText: {
+        type: String,
+        default: '取消'
+      },
+      showCancelButton: Boolean,
+      showConfirmButton: {
+        type: Boolean,
+        default: true
+      },
+      overlay: {
+        type: Boolean,
+        default: true
+      },
+      closeOnClickOverlay: {
+        type: Boolean,
+        default: false
+      },
+      lockOnScroll: {
+        type: Boolean,
+        default: true
+      }
+    },
   
-  &__header {
-    padding: 15px 0 0;
-    text-align: center;
-  }
-
-  &__content {
-    &::after {
-      border-bottom-width: 1px;
-    }
-  }
-
-  &__message {
-    line-height: 1.5;
-    padding: 15px 20px;
-
-    &--withtitle {
-      color: #ccc;
-      font-size: 14px;
-    }
-  }
-
-  &__footer {
-    overflow: hidden;
-
-    &.is-twobtn {
-      display: flex;
-
-      .van-button {
-        flex: 1;
+    methods: {
+      handleAction(action) {
+        this.$emit('input', false);
+        this.$emit(action);
+        this.callback && this.callback(action);
       }
     }
-  }
-
-  .van-button {
-    border: 0;
-  }
-
-  &__confirm {
-    &,
-    &:active {
-      color: #00C000;
-    }
-  }
-
-  &-bounce-enter {
-    opacity: 0;
-    transform: translate3d(-50%, -50%, 0) scale(0.7);
-  }
-
-  &-bounce-leave-active {
-    opacity: 0;
-    transform: translate3d(-50%, -50%, 0) scale(0.9);
-  }
-}
-
-</style>
+  };
+</script>
