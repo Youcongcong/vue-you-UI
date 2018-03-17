3a71492fa8ebc093d27ae69f320f435b9e4a0f17
youcongcong <834153504@qq.com>
2018-03-17 15:53:31 +0800

    modify:pg

--- examples/components/PageTransition.vue
+++ examples/components/PageTransition.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-  <transition :name="transitionName">
-    <router-view class="child-view"></router-view>
-  </transition>
+    <transition :name="transitionName">
+      <router-view class="child-view"></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
+    data() {
       return {
         transitionName: 'slide-left'
       }
     },
-    beforeRouteUpdate (to, from, next) {
+    beforeRouteUpdate(to, from, next) {
       let isBack = this.$router.isBack
       if (isBack) {
         this.transitionName = 'slide-right'
@@ -28,25 +28,30 @@
 
 <style scoped>
   .child-view {
-  position: absolute;
-  width:100%;
-  height:100%;
-  transition: all .8s cubic-bezier(.55,0,.1,1);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transition: all .8s cubic-bezier(.55, 0, .1, 1);
   }
-  .slide-left-enter, .slide-right-leave-active {
+  
+  .slide-left-enter,
+  .slide-right-leave-active {
     opacity: 0;
     -webkit-transform: translate(50px, 0);
     transform: translate(50px, 0);
   }
-  .slide-left-leave-active, .slide-right-enter {
+  
+  .slide-left-leave-active,
+  .slide-right-enter {
     opacity: 0;
     -webkit-transform: translate(-50px, 0);
     transform: translate(-50px, 0);
   }
+  
   .header {
-    position:absolute;
-    height:44px;
-    background:#0058f1;
-    width:100%
+    position: absolute;
+    height: 44px;
+    background: #0058f1;
+    width: 100%
   }
 </style>
